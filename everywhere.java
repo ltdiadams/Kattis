2196f84f1fa639377f8b0c33e38b60e356018b61
Logan DiAdams <ltdiadams@mta.ca>
2019-01-29 00:23:26 -0400

    Testing commit from atom with Kattis

--- everywhere.java
+++ everywhere.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 //use an array
 
-public class everywhere{
+public class everywhere {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
-    Scanner scan = new Scanner (System.in);
+    Scanner scan = new Scanner(System.in);
     int testCases = scan.nextInt();
     int count = 0;
 
-    for (int i = 0; i < testCases; i++){
+    for (int i = 0; i < testCases; i++) {
       int n = scan.nextInt();
 
-      while (i < n){
+      while (i < n) {
         String name = scan.nextLine();
-        if (scan.nextLine() != name){
+        if (scan.nextLine() != name) {
           count++;
+          System.out.println("to test the git!!");
 
-        }
-        else{
+        } else {
           //scan.nextLine();
         }
       }
